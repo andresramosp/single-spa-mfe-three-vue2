07ce0859d60df605367eba7d35766052f26b4d14
Andrés Ramos Palacios <andreschennai@gmail.com>
2022-03-11 19:29:07 -0600

    changes

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: { port: 9995 },
+  configureWebpack: {
+    output: {
+      libraryTarget: 'system'
+    }
+  }
 })
